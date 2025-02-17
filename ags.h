13c2736ebf088010e1d2204992cc70ac0cfc9eec
6713221 จักริน <jakkarin.roe@student.mahidol.edu>
2025-02-17 09:51:07 +0000

    y

--- ags.h
+++ ags.h
@@ -167,7 +167,7 @@ void printListR( LLPtr currentPtr )
       while(currentPtr->nextPtr!=NULL)currentPtr=currentPtr->nextPtr;
       // while not the end of the list
       while(currentPtr->pPtr!=NULL) {
-         printf( " %d %s -->", currentPtr->id,currentPtr->name );
+         printf( "%d %s -->", currentPtr->id,currentPtr->name );
          currentPtr = currentPtr->pPtr;
       } // end while
 
